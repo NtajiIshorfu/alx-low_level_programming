41cf0c36d572b041c591a4b00720f76479619648
Onyemaechi Faith <ioriginforus@gmail.com>
2022-09-12 14:33:43 -0700

    v

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void) #no arguments
+/**
+ * main - prints an integer
+ *
+ * Description: using the main function
+ * Return:0
+*/
+int main(void)
 {
 char Mystr[] = "\"Programming is like building a multilingual puzzle";
 puts(Mystr);
