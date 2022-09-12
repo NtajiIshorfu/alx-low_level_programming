4629f981a9c05432f0b8f3e6235f4f2717848a0b
Onyemaechi Faith <ioriginforus@gmail.com>
2022-09-12 14:42:04 -0700

    k

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints out void
+ *
+ * Description: prints out a certain text
+ * Return:0
+*/
 int main(void)
 {
 printf("with proper grammer, but the outcome is a piece of art,\n");
